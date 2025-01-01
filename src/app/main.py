f726773bf25f3d38159a639d1d55c076d244a024
Roan <roan1segers@gmail.com>
2025-01-01 15:35:10 +0100

    changed import of info

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from fetch import fashionFetch
-import privateinfo 
+from privateinfo import info
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -35,4 +35,4 @@ async def goodnight(ctx):
     await ctx.channel.send("Goodnight bro, love is real")
 
 
-client.run(privateinfo.info.botcode)
+client.run(info.botcode)
